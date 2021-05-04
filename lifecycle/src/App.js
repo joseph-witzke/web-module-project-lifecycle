b94b2323e71fb140c6c038c660c4d812b9ea7bbb
Joe Witzke <joewitzke@joes-mbp.lan>
2021-05-04 13:54:31 -0600

    reciving user data from api

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Lifecycles</h1>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    name: "",
+    avatar_url: ""
+  }
+  componentDidMount() {
+    axios.get('https://api.github.com/users/joseph-witzke')
+      .then(res => {
+        console.log(res)
+        this.setState(res.data)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Lifecycles</h1>
+      </div>
+    );
+  } 
 }
 
 export default App;
